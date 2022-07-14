05a32ee86278f6370ca1210b59042595454ffbbf
Francisco Perez <perezbenzif@gmail.com>
2022-07-14 09:46:42 -0300

    segunda entrega

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="Navbar">
+      <div>Álvarez</div>
+      <div>
+        <button>Inicio</button>
+        <button>Contacto</button>
+        <button>Nosotros</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
